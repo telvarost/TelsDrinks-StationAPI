cce4e9f1ddd8b083ad49cdff015d4c86ba2903a9
telvarost <telontelvarost@gmail.com>
2024-10-25 18:00:14 -0400

    Add new recipes

--- src/main/java/com/github/telvarost/telsdrinks/events/RecipeListener.java
+++ src/main/java/com/github/telvarost/telsdrinks/events/RecipeListener.java
@@ -8,8 +8,7 @@ import net.modificationstation.stationapi.api.recipe.CraftingRegistry;
 import net.modificationstation.stationapi.api.recipe.SmeltingRegistry;
 import net.modificationstation.stationapi.api.util.Identifier;
 
-import static com.github.telvarost.telsdrinks.events.BlockListener.EMPTY_MUG;
-import static com.github.telvarost.telsdrinks.events.BlockListener.KETTLE;
+import static com.github.telvarost.telsdrinks.events.BlockListener.*;
 import static com.github.telvarost.telsdrinks.events.ItemListener.CLAY_MUG;
 
 public class RecipeListener {
@@ -20,6 +19,8 @@ public class RecipeListener {
 
         if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPED.type()) {
             CraftingRegistry.addShapedRecipe(new ItemStack(KETTLE.asItem(), 1), " X ", "X X", "XXX", 'X', Item.IRON_INGOT);
+            CraftingRegistry.addShapedRecipe(new ItemStack(WATER_KETTLE.asItem(), 1), " X ", "XYX", "XXX", 'X', Item.IRON_INGOT, 'Y', Item.WATER_BUCKET);
+            CraftingRegistry.addShapedRecipe(new ItemStack(MILK_KETTLE.asItem(), 1), " X ", "XYX", "XXX", 'X', Item.IRON_INGOT, 'Y', Item.MILK_BUCKET);
             CraftingRegistry.addShapedRecipe(new ItemStack(CLAY_MUG.asItem(), 2), "   ", "XXX", " X ", 'X', Item.CLAY);
         }
 
@@ -28,6 +29,14 @@ public class RecipeListener {
         }
 
         if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPELESS.type()) {
+            CraftingRegistry.addShapelessRecipe(new ItemStack(KETTLE.asItem(), 1), new ItemStack(WATER_KETTLE.asItem(), 1));
+            CraftingRegistry.addShapelessRecipe(new ItemStack(KETTLE.asItem(), 1), new ItemStack(MILK_KETTLE.asItem(), 1));
+            CraftingRegistry.addShapelessRecipe(new ItemStack(KETTLE.asItem(), 1), new ItemStack(BITTER_KETTLE.asItem(), 1));
+            CraftingRegistry.addShapelessRecipe(new ItemStack(KETTLE.asItem(), 1), new ItemStack(APPLE_KETTLE.asItem(), 1));
+            CraftingRegistry.addShapelessRecipe(new ItemStack(WATER_KETTLE.asItem(), 1), new ItemStack(KETTLE.asItem(), 1), Item.WATER_BUCKET);
+            CraftingRegistry.addShapelessRecipe(new ItemStack(MILK_KETTLE.asItem(), 1), new ItemStack(KETTLE.asItem(), 1), Item.MILK_BUCKET);
+            CraftingRegistry.addShapelessRecipe(new ItemStack(APPLE_KETTLE.asItem(), 1), new ItemStack(WATER_KETTLE.asItem(), 1), Item.APPLE);
+            CraftingRegistry.addShapelessRecipe(new ItemStack(BITTER_KETTLE.asItem(), 1), new ItemStack(WATER_KETTLE.asItem(), 1), new ItemStack(Item.DYE, 1, 3));
         }
     }
 }
