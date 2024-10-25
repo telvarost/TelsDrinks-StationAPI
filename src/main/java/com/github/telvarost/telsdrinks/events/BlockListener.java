48cab3faa4a126b26afcbba152779f4f9e418b2d
telvarost <telontelvarost@gmail.com>
2024-10-24 21:53:59 -0400

    Add different kettle types

--- src/main/java/com/github/telvarost/telsdrinks/events/BlockListener.java
+++ src/main/java/com/github/telvarost/telsdrinks/events/BlockListener.java
@@ -17,6 +17,10 @@ public class BlockListener {
 
     public static Block[] blocks;
     public static Block KETTLE;
+    public static Block APPLE_KETTLE;
+    public static Block BITTER_KETTLE;
+    public static Block MILK_KETTLE;
+    public static Block WATER_KETTLE;
     public static Block MUG;
 
     @Entrypoint.Namespace
@@ -26,10 +30,18 @@ public class BlockListener {
     public void registerBlocks(BlockRegistryEvent event) {
 
         KETTLE = new Kettle(Identifier.of(NAMESPACE, "kettle"), Material.CLAY).setSoundGroup(METAL_SOUND_GROUP).setTranslationKey(NAMESPACE, "kettle");
+        APPLE_KETTLE = new Kettle(Identifier.of(NAMESPACE, "apple_kettle"), Material.CLAY).setSoundGroup(METAL_SOUND_GROUP).setTranslationKey(NAMESPACE, "apple_kettle");
+        BITTER_KETTLE = new Kettle(Identifier.of(NAMESPACE, "bitter_kettle"), Material.CLAY).setSoundGroup(METAL_SOUND_GROUP).setTranslationKey(NAMESPACE, "bitter_kettle");
+        MILK_KETTLE = new Kettle(Identifier.of(NAMESPACE, "milk_kettle"), Material.CLAY).setSoundGroup(METAL_SOUND_GROUP).setTranslationKey(NAMESPACE, "milk_kettle");
+        WATER_KETTLE = new Kettle(Identifier.of(NAMESPACE, "water_kettle"), Material.CLAY).setSoundGroup(METAL_SOUND_GROUP).setTranslationKey(NAMESPACE, "water_kettle");
         MUG = new Mug(Identifier.of(NAMESPACE, "mug"), Material.CLAY).setSoundGroup(STONE_SOUND_GROUP).setTranslationKey(NAMESPACE, "mug");
 
         blocks = new Block[]
         {   KETTLE
+        ,   APPLE_KETTLE
+        ,   BITTER_KETTLE
+        ,   MILK_KETTLE
+        ,   WATER_KETTLE
         ,   MUG
         };
     }
