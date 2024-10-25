1835a5247f48bf0063384535b0f1ef3634b2d236
matthewperiut <matthewperiut@gmail.com>
2024-10-25 19:22:18 -0400

    Kettles with Liquid

--- src/main/java/com/github/telvarost/telsdrinks/blockentity/KettleBlockEntity.java
+++ src/main/java/com/github/telvarost/telsdrinks/blockentity/KettleBlockEntity.java
@@ -1,15 +1,19 @@
 package com.github.telvarost.telsdrinks.blockentity;
 
-import com.github.telvarost.telsdrinks.block.Kettle;
+import com.github.telvarost.telsdrinks.events.BlockListener;
 import net.minecraft.block.entity.BlockEntity;
-import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.state.property.Properties;
 import net.modificationstation.stationapi.api.util.math.Direction;
 
 
 public class KettleBlockEntity extends BlockEntity {
-    public boolean dirXSet = false;
+    public boolean initialized = false;
     public boolean dirX = false;
+
+    public int liquidHorizontalOffset = 0;
+
+    public int tempTickCounter = 0;
+    public int liquidLevel = 0;
 
     public KettleBlockEntity() {
 
@@ -17,15 +21,51 @@ public class KettleBlockEntity extends BlockEntity {
 
     @Override
     public void tick() {
-        if (!dirXSet) {
+        if (!initialized) {
             if (world != null) {
                 Direction dir = world.getBlockState(x, y, z).get(Properties.HORIZONTAL_FACING);
                 if (dir == Direction.EAST || dir == Direction.WEST) {
                     dirX = true;
-                    dirXSet = true;
                 }
+
+                final int poison = BlockListener.KETTLE.id;
+                final int water = BlockListener.WATER_KETTLE.id;
+                final int milk = BlockListener.MILK_KETTLE.id;
+                final int bitter = BlockListener.BITTER_KETTLE.id;
+                final int apple = BlockListener.APPLE_KETTLE.id;
+
+                int blockId = world.getBlockId(x,y,z);
+
+                if (blockId == poison) {
+                    liquidHorizontalOffset = 0;
+                }
+                if (blockId == water) {
+                    liquidHorizontalOffset = 2;
+                }
+                if (blockId == milk) {
+                    liquidHorizontalOffset = 4;
+                }
+                if (blockId == bitter) {
+                    liquidHorizontalOffset = 6;
+                }
+                if (blockId == apple) {
+                    liquidHorizontalOffset = 8;
+                }
+
+                initialized = true;
             }
         }
+
+        tempTickCounter++;
+        if (tempTickCounter > 40) {
+            liquidLevel++;
+            if (liquidLevel > 5) {
+                liquidLevel = 0;
+            }
+            tempTickCounter = 0;
+        }
+
+
         super.tick();
     }
 }
--- src/main/java/com/github/telvarost/telsdrinks/client/render/blockentity/KettleBlockEntityRenderer.java
+++ src/main/java/com/github/telvarost/telsdrinks/client/render/blockentity/KettleBlockEntityRenderer.java
@@ -19,8 +19,6 @@ public class KettleBlockEntityRenderer extends BlockEntityRenderer {
     public void render(BlockEntity blockEntity, double x, double y, double z, float tickDelta) {
         GL11.glPushMatrix();
 
-
-
         // Position the kettle
         GL11.glTranslatef((float) x, (float) y, (float) z);
         GL11.glDisable(GL11.GL_LIGHTING);
@@ -35,54 +33,73 @@ public class KettleBlockEntityRenderer extends BlockEntityRenderer {
         GL11.glScalef(scale, -scale, -scale);
 
         // set offset to the horizontal pixel offset for your texture
-        double offset = 0;
 
         double HALF_PIXEL_OFFSET = 0.03125;
         double Z_FIGHT_OFFSET = 0.001F;
+
+        double LEFT = 10.F/16.F + HALF_PIXEL_OFFSET;
+        double RIGHT = 13.F/16.F + HALF_PIXEL_OFFSET;
+        double BOTTOM = 4.F/16.F + HALF_PIXEL_OFFSET;
+        double TOP = (12.F/16.F);
+
+        double TEXTURE_WIDTH = 2;
+        double TEXTURE_HEIGHT = 5;
+
+//        double LEFT = 9.F/16.F;
+//        double RIGHT = 14.F/16.F + 0.06125;
+//        double BOTTOM = 4.F/16.F;
+//        double TOP = (12.F/16.F);
+//
+//        double TEXTURE_WIDTH = 4;
+//        double TEXTURE_HEIGHT = 5;
+
+//        double LEFT = 9.F/16.F;
+//        double RIGHT = 14.F/16.F + 0.06125;
+//        double BOTTOM = 3.F/16.F;
+//        double TOP = (12.F/16.F);
+//
+//        double TEXTURE_WIDTH = 4;
+//        double TEXTURE_HEIGHT = 6;
+
+        double uv_x_offset = ((KettleBlockEntity) blockEntity).liquidHorizontalOffset;;
+        int uv_y_offset = ((KettleBlockEntity) blockEntity).liquidLevel;
 
         //double Z_FIGHT_OFFSET = 0.001;
         if (((KettleBlockEntity) blockEntity).dirX) {
 
             double POSX_BASE = 19.F/16.F + HALF_PIXEL_OFFSET + Z_FIGHT_OFFSET;
             double NEGX_BASE = 5.F/16.F - HALF_PIXEL_OFFSET - Z_FIGHT_OFFSET;
-            double LEFT = 9.F/16.F + HALF_PIXEL_OFFSET;
-            double RIGHT = 14.F/16.F + HALF_PIXEL_OFFSET;
-            double BOTTOM = 3.F/16.F;
-            double TOP = (12.F/16.F);
 
             Tessellator tessellator = Tessellator.INSTANCE;
             tessellator.startQuads();
 
-            tessellator.vertex(POSX_BASE, -TOP, -RIGHT, (4 + offset) / 64.0F, 0 / 32.0F);
-            tessellator.vertex(POSX_BASE, -BOTTOM, -RIGHT, (4 + offset) / 64.0F, 6 / 32.0F);
-            tessellator.vertex(POSX_BASE, -BOTTOM, -LEFT, (0 + offset) / 64.0F, 6 / 32.0F);
-            tessellator.vertex(POSX_BASE, -TOP, -LEFT, (0 + offset) / 64.0F, 0 / 32.0F);
+            tessellator.vertex(POSX_BASE, -TOP, -RIGHT, (TEXTURE_WIDTH + uv_x_offset) / 64.0F, (uv_y_offset) / 32.0F);
+            tessellator.vertex(POSX_BASE, -BOTTOM, -RIGHT, (TEXTURE_WIDTH + uv_x_offset) / 64.0F, (TEXTURE_HEIGHT + uv_y_offset) / 32.0F);
+            tessellator.vertex(POSX_BASE, -BOTTOM, -LEFT, (0 + uv_x_offset) / 64.0F, (TEXTURE_HEIGHT + uv_y_offset) / 32.0F);
+            tessellator.vertex(POSX_BASE, -TOP, -LEFT, (0 + uv_x_offset) / 64.0F, uv_y_offset / 32.0F);
 
-            tessellator.vertex(NEGX_BASE, -TOP, -LEFT, (0 + offset) / 64.0F, 0 / 32.0F);
-            tessellator.vertex(NEGX_BASE, -BOTTOM, -LEFT, (0 + offset) / 64.0F, 6 / 32.0F);
-            tessellator.vertex(NEGX_BASE, -BOTTOM, -RIGHT, (4 + offset) / 64.0F, 6 / 32.0F);
-            tessellator.vertex(NEGX_BASE, -TOP, -RIGHT, (4 + offset) / 64.0F, 0 / 32.0F);
+            tessellator.vertex(NEGX_BASE, -TOP, -LEFT, (0 + uv_x_offset) / 64.0F, uv_y_offset / 32.0F);
+            tessellator.vertex(NEGX_BASE, -BOTTOM, -LEFT, (0 + uv_x_offset) / 64.0F, (TEXTURE_HEIGHT + uv_y_offset) / 32.0F);
+            tessellator.vertex(NEGX_BASE, -BOTTOM, -RIGHT, (TEXTURE_WIDTH + uv_x_offset) / 64.0F, (TEXTURE_HEIGHT + uv_y_offset) / 32.0F);
+            tessellator.vertex(NEGX_BASE, -TOP, -RIGHT, (TEXTURE_WIDTH + uv_x_offset) / 64.0F, uv_y_offset / 32.0F);
 
             tessellator.draw();
         } else {
             double POSZ_BASE = 19.F/16.F + HALF_PIXEL_OFFSET + Z_FIGHT_OFFSET - 1.5;
             double NEGZ_BASE = 5.F/16.F - HALF_PIXEL_OFFSET - Z_FIGHT_OFFSET - 1.5;
-            double LEFT = 9.F/16.F + HALF_PIXEL_OFFSET;
-            double RIGHT = 14.F/16.F + HALF_PIXEL_OFFSET;
-            double BOTTOM = 3.F/16.F;
-            double TOP = (12.F/16.F);
+
 
             Tessellator tessellator = Tessellator.INSTANCE;
             tessellator.startQuads();
-            tessellator.vertex(LEFT, -TOP, NEGZ_BASE, (0 + offset) / 64.0F, 0 / 32.0F);
-            tessellator.vertex(LEFT, -BOTTOM, NEGZ_BASE, (0 + offset) / 64.0F, 6 / 32.0F);
-            tessellator.vertex(RIGHT, -BOTTOM, NEGZ_BASE, (4 + offset) / 64.0F, 6 / 32.0F);
-            tessellator.vertex(RIGHT, -TOP, NEGZ_BASE, (4 + offset) / 64.0F, 0 / 32.0F);
+            tessellator.vertex(LEFT, -TOP, NEGZ_BASE, (0 + uv_x_offset) / 64.0F, uv_y_offset / 32.0F);
+            tessellator.vertex(LEFT, -BOTTOM, NEGZ_BASE, (0 + uv_x_offset) / 64.0F, (TEXTURE_HEIGHT + uv_y_offset) / 32.0F);
+            tessellator.vertex(RIGHT, -BOTTOM, NEGZ_BASE, (TEXTURE_WIDTH + uv_x_offset) / 64.0F, (TEXTURE_HEIGHT + uv_y_offset) / 32.0F);
+            tessellator.vertex(RIGHT, -TOP, NEGZ_BASE, (TEXTURE_WIDTH + uv_x_offset) / 64.0F, uv_y_offset / 32.0F);
 
-            tessellator.vertex(RIGHT, -TOP, POSZ_BASE, (4 + offset) / 64.0F, 0 / 32.0F);
-            tessellator.vertex(RIGHT, -BOTTOM, POSZ_BASE, (4 + offset) / 64.0F, 6 / 32.0F);
-            tessellator.vertex(LEFT, -BOTTOM, POSZ_BASE, (0 + offset) / 64.0F, 6 / 32.0F);
-            tessellator.vertex(LEFT, -TOP, POSZ_BASE, (0 + offset) / 64.0F, 0 / 32.0F);
+            tessellator.vertex(RIGHT, -TOP, POSZ_BASE, (TEXTURE_WIDTH + uv_x_offset) / 64.0F, uv_y_offset / 32.0F);
+            tessellator.vertex(RIGHT, -BOTTOM, POSZ_BASE, (TEXTURE_WIDTH + uv_x_offset) / 64.0F, (TEXTURE_HEIGHT + uv_y_offset) / 32.0F);
+            tessellator.vertex(LEFT, -BOTTOM, POSZ_BASE, (0 + uv_x_offset) / 64.0F, (TEXTURE_HEIGHT + uv_y_offset) / 32.0F);
+            tessellator.vertex(LEFT, -TOP, POSZ_BASE, (0 + uv_x_offset) / 64.0F, uv_y_offset / 32.0F);
 
             tessellator.draw();
         }
