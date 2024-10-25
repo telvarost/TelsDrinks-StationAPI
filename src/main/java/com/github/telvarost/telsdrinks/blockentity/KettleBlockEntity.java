de827cec4c94d94f9f2bec9b1aff2cab7dd15afc
matthewperiut <matthewperiut@gmail.com>
2024-10-25 18:37:23 -0400

    Kettles with Water

--- src/main/java/com/github/telvarost/telsdrinks/blockentity/KettleBlockEntity.java
+++ src/main/java/com/github/telvarost/telsdrinks/blockentity/KettleBlockEntity.java
@@ -1,7 +1,31 @@
 package com.github.telvarost.telsdrinks.blockentity;
 
+import com.github.telvarost.telsdrinks.block.Kettle;
 import net.minecraft.block.entity.BlockEntity;
+import net.modificationstation.stationapi.api.block.BlockState;
+import net.modificationstation.stationapi.api.state.property.Properties;
+import net.modificationstation.stationapi.api.util.math.Direction;
+
 
 public class KettleBlockEntity extends BlockEntity {
+    public boolean dirXSet = false;
+    public boolean dirX = false;
 
+    public KettleBlockEntity() {
+
+    }
+
+    @Override
+    public void tick() {
+        if (!dirXSet) {
+            if (world != null) {
+                Direction dir = world.getBlockState(x, y, z).get(Properties.HORIZONTAL_FACING);
+                if (dir == Direction.EAST || dir == Direction.WEST) {
+                    dirX = true;
+                    dirXSet = true;
+                }
+            }
+        }
+        super.tick();
+    }
 }
--- src/main/java/com/github/telvarost/telsdrinks/client/render/blockentity/KettleBlockEntityRenderer.java
+++ src/main/java/com/github/telvarost/telsdrinks/client/render/blockentity/KettleBlockEntityRenderer.java
@@ -1,41 +1,96 @@
 package com.github.telvarost.telsdrinks.client.render.blockentity;
-
+import com.github.telvarost.telsdrinks.blockentity.KettleBlockEntity;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
-import net.minecraft.client.render.block.entity.PistonBlockEntityRenderer;
 import org.lwjgl.opengl.GL11;
 
+@Environment(EnvType.CLIENT)
 public class KettleBlockEntityRenderer extends BlockEntityRenderer {
+
+    public KettleBlockEntityRenderer() {
+    }
+
     @Override
     public void render(BlockEntity blockEntity, double x, double y, double z, float tickDelta) {
-        Tessellator tessellator = Tessellator.INSTANCE;
-
-        // Set up OpenGL state
         GL11.glPushMatrix();
-        GL11.glTranslated(x, y - 1, z); // Move to the correct position
+
+
+
+        // Position the kettle
+        GL11.glTranslatef((float) x, (float) y, (float) z);
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
-        // Begin drawing quads
-        tessellator.startQuads();
+        // Bind the texture
+        this.bindTexture("/assets/telsdrinks/blockentity/liquids.png");
 
-        // Set color to blue
-        tessellator.color(0.0F, 0.0F, 1.0F, 1.0F); // RGBA
+        // Scale and render the model
+        float scale = 0.6666667F; // Adjust scale as necessary
+        GL11.glScalef(scale, -scale, -scale);
 
-        // Define the vertices for a rectangle (quad)
-        // Adjust coordinates as necessary for size and positioning
-        tessellator.vertex(-0.5, 0.0, -0.5);
-        tessellator.vertex(0.5, 0.0, -0.5);
-        tessellator.vertex(0.5, 0.0, 0.5);
-        tessellator.vertex(-0.5, 0.0, 0.5);
+        // set offset to the horizontal pixel offset for your texture
+        double offset = 0;
 
-        // Draw the rectangle
-        tessellator.draw();
+        double HALF_PIXEL_OFFSET = 0.03125;
+        double Z_FIGHT_OFFSET = 0.001F;
+
+        //double Z_FIGHT_OFFSET = 0.001;
+        if (((KettleBlockEntity) blockEntity).dirX) {
+
+            double POSX_BASE = 19.F/16.F + HALF_PIXEL_OFFSET + Z_FIGHT_OFFSET;
+            double NEGX_BASE = 5.F/16.F - HALF_PIXEL_OFFSET - Z_FIGHT_OFFSET;
+            double LEFT = 9.F/16.F + HALF_PIXEL_OFFSET;
+            double RIGHT = 14.F/16.F + HALF_PIXEL_OFFSET;
+            double BOTTOM = 3.F/16.F;
+            double TOP = (12.F/16.F);
+
+            Tessellator tessellator = Tessellator.INSTANCE;
+            tessellator.startQuads();
+
+            tessellator.vertex(POSX_BASE, -TOP, -RIGHT, (4 + offset) / 64.0F, 0 / 32.0F);
+            tessellator.vertex(POSX_BASE, -BOTTOM, -RIGHT, (4 + offset) / 64.0F, 6 / 32.0F);
+            tessellator.vertex(POSX_BASE, -BOTTOM, -LEFT, (0 + offset) / 64.0F, 6 / 32.0F);
+            tessellator.vertex(POSX_BASE, -TOP, -LEFT, (0 + offset) / 64.0F, 0 / 32.0F);
+
+            tessellator.vertex(NEGX_BASE, -TOP, -LEFT, (0 + offset) / 64.0F, 0 / 32.0F);
+            tessellator.vertex(NEGX_BASE, -BOTTOM, -LEFT, (0 + offset) / 64.0F, 6 / 32.0F);
+            tessellator.vertex(NEGX_BASE, -BOTTOM, -RIGHT, (4 + offset) / 64.0F, 6 / 32.0F);
+            tessellator.vertex(NEGX_BASE, -TOP, -RIGHT, (4 + offset) / 64.0F, 0 / 32.0F);
+
+            tessellator.draw();
+        } else {
+            double POSZ_BASE = 19.F/16.F + HALF_PIXEL_OFFSET + Z_FIGHT_OFFSET - 1.5;
+            double NEGZ_BASE = 5.F/16.F - HALF_PIXEL_OFFSET - Z_FIGHT_OFFSET - 1.5;
+            double LEFT = 9.F/16.F + HALF_PIXEL_OFFSET;
+            double RIGHT = 14.F/16.F + HALF_PIXEL_OFFSET;
+            double BOTTOM = 3.F/16.F;
+            double TOP = (12.F/16.F);
+
+            Tessellator tessellator = Tessellator.INSTANCE;
+            tessellator.startQuads();
+            tessellator.vertex(LEFT, -TOP, NEGZ_BASE, (0 + offset) / 64.0F, 0 / 32.0F);
+            tessellator.vertex(LEFT, -BOTTOM, NEGZ_BASE, (0 + offset) / 64.0F, 6 / 32.0F);
+            tessellator.vertex(RIGHT, -BOTTOM, NEGZ_BASE, (4 + offset) / 64.0F, 6 / 32.0F);
+            tessellator.vertex(RIGHT, -TOP, NEGZ_BASE, (4 + offset) / 64.0F, 0 / 32.0F);
+
+            tessellator.vertex(RIGHT, -TOP, POSZ_BASE, (4 + offset) / 64.0F, 0 / 32.0F);
+            tessellator.vertex(RIGHT, -BOTTOM, POSZ_BASE, (4 + offset) / 64.0F, 6 / 32.0F);
+            tessellator.vertex(LEFT, -BOTTOM, POSZ_BASE, (0 + offset) / 64.0F, 6 / 32.0F);
+            tessellator.vertex(LEFT, -TOP, POSZ_BASE, (0 + offset) / 64.0F, 0 / 32.0F);
+
+            tessellator.draw();
+        }
+
 
         // Restore OpenGL state
         GL11.glEnable(GL11.GL_LIGHTING);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F); // Reset color
         GL11.glPopMatrix();
     }
 }
