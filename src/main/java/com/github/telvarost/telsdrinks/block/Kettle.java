76dda650f57c5d7fe4ce4527ea64a2f8a09791c8
telvarost <telontelvarost@gmail.com>
2024-10-27 00:15:38 -0400

    Add ability to take out water and milk from kettles and put water and milk into kettles

--- src/main/java/com/github/telvarost/telsdrinks/block/Kettle.java
+++ src/main/java/com/github/telvarost/telsdrinks/block/Kettle.java
@@ -2,18 +2,20 @@ package com.github.telvarost.telsdrinks.block;
 
 import com.github.telvarost.telsdrinks.blockentity.KettleBlockEntity;
 import com.github.telvarost.telsdrinks.events.BlockListener;
-import com.github.telvarost.telsdrinks.events.ItemListener;
 import com.github.telvarost.telsdrinks.item.KettleBlockItem;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.block.HasCustomBlockItemFactory;
 import net.modificationstation.stationapi.api.item.ItemPlacementContext;
 import net.modificationstation.stationapi.api.state.StateManager;
 import net.modificationstation.stationapi.api.state.property.Properties;
-import net.modificationstation.stationapi.api.template.block.TemplateBlock;
 import net.modificationstation.stationapi.api.template.block.TemplateBlockWithEntity;
 import net.modificationstation.stationapi.api.util.Identifier;
 import net.modificationstation.stationapi.api.util.math.Direction;
@@ -68,5 +70,80 @@ public class Kettle extends TemplateBlockWithEntity {
     @Override
     public int getDroppedItemId(int blockMeta, Random random) {
         return BlockListener.KETTLE.id;
+    }
+
+    @Override
+    public boolean onUse(World world, int x, int y, int z, PlayerEntity player) {
+        int blockId = world.getBlockId(x, y, z);
+        ItemStack heldItem = player.inventory.getSelectedItem();
+        KettleBlockEntity kettleBlockEntity = (KettleBlockEntity)world.getBlockEntity(x, y, z);
+
+        if (null != heldItem) {
+            if (  (  blockId == BlockListener.KETTLE.id
+                  || (blockId == BlockListener.WATER_KETTLE.id && kettleBlockEntity.liquidLevel < 5)
+                  || kettleBlockEntity.liquidLevel == 0
+                  )
+               && heldItem.itemId == Item.WATER_BUCKET.id
+            ) {
+                player.inventory.setStack(player.inventory.selectedSlot, new ItemStack(Item.BUCKET));
+                Direction direction = world.getBlockState(x, y, z).get(Properties.HORIZONTAL_FACING);
+                world.setBlock(x, y, z, BlockListener.WATER_KETTLE.id);
+                BlockState blockState = world.getBlockState(x, y, z);
+                world.setBlockState(x, y, z, blockState.with(Properties.HORIZONTAL_FACING, direction));
+                return true;
+            } else if (  (  blockId == BlockListener.KETTLE.id
+                         || (blockId == BlockListener.MILK_KETTLE.id && kettleBlockEntity.liquidLevel < 5)
+                         || kettleBlockEntity.liquidLevel == 0
+                         )
+                      && heldItem.itemId == Item.MILK_BUCKET.id
+            ) {
+                player.inventory.setStack(player.inventory.selectedSlot, new ItemStack(Item.BUCKET));
+                Direction direction =  world.getBlockState(x,y,z).get(Properties.HORIZONTAL_FACING);
+                world.setBlock(x, y, z, BlockListener.MILK_KETTLE.id);
+                BlockState blockState = world.getBlockState(x, y, z);
+                world.setBlockState(x, y, z, blockState.with(Properties.HORIZONTAL_FACING, direction));
+                return true;
+            } else if (blockId == BlockListener.WATER_KETTLE.id && heldItem.itemId == Item.APPLE.id) {
+                if (heldItem.count == 1) {
+                    player.inventory.setStack(player.inventory.selectedSlot, null);
+                } else {
+                    heldItem.count--;
+                }
+                Direction direction =  world.getBlockState(x,y,z).get(Properties.HORIZONTAL_FACING);
+                world.setBlock(x, y, z, BlockListener.APPLE_KETTLE.id);
+                BlockState blockState = world.getBlockState(x, y, z);
+                world.setBlockState(x, y, z, blockState.with(Properties.HORIZONTAL_FACING, direction));
+                return true;
+            } else if (blockId == BlockListener.WATER_KETTLE.id && heldItem.itemId == Item.DYE.id && heldItem.getDamage() == 3) {
+                if (heldItem.count == 1) {
+                    player.inventory.setStack(player.inventory.selectedSlot, null);
+                } else {
+                    heldItem.count--;
+                }
+                Direction direction =  world.getBlockState(x,y,z).get(Properties.HORIZONTAL_FACING);
+                world.setBlock(x, y, z, BlockListener.BITTER_KETTLE.id);
+                BlockState blockState = world.getBlockState(x, y, z);
+                world.setBlockState(x, y, z, blockState.with(Properties.HORIZONTAL_FACING, direction));
+                return true;
+            } else if (  heldItem.itemId == Item.BUCKET.id
+                      && kettleBlockEntity.liquidLevel == 5
+            ) {
+                if (blockId == BlockListener.WATER_KETTLE.id) {
+                    player.inventory.setStack(player.inventory.selectedSlot, new ItemStack(Item.WATER_BUCKET));
+                    kettleBlockEntity.takeLiquidOut();
+                    kettleBlockEntity.takeLiquidOut();
+                    kettleBlockEntity.takeLiquidOut();
+                    return true;
+                } else if (blockId == BlockListener.MILK_KETTLE.id) {
+                    player.inventory.setStack(player.inventory.selectedSlot, new ItemStack(Item.MILK_BUCKET));
+                    kettleBlockEntity.takeLiquidOut();
+                    kettleBlockEntity.takeLiquidOut();
+                    kettleBlockEntity.takeLiquidOut();
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
